33fb4164f95235dd0a6da49ac922c86647321b8c
hainghiem <hainghiem375@gmail.com>
2023-04-29 14:03:42 -0400

    separated things out into consts

---
+++ consts.py
@@ -0,0 +1,48 @@
+company_handbook_faiss_path = "./faiss_gitlab_handbook"
+
+llm_model_type = "gpt-3.5-turbo"
+
+# `thinking_thoughts` is a list of phrases that can be used to indicate that the bot is thinking
+thinking_thoughts = [
+    "Mental gears turning ⚙️...",
+    "Thinking cap on 🤠...",
+    "Brainstorming 🧠...",
+    "Calculating 👩‍💻...",
+    "Idea brewing 💡...",
+    "Mulling over 🧠...",
+    "Brain waves flowing 🧠...",
+    "Mind churning 🤔...",
+    "Mental gymnastics...",
+    "Pondering deeply 💭...",
+    "Musing 🤔...",
+    "Contemplating 😊...",
+    "Ruminating 🙂...",
+    "Processing 🤔...",
+    "Chewing on it...",
+    "Reflecting 🤔...",
+    "Daydreaming ☁️..."
+]
+
+# `learn_more_phrases` is a list of phrases that can be used to separate the answer from the list of sources
+learn_more_phrases = [
+    "For more information, check out these resources",
+    "To learn more, explore these resources",
+    "To find out more, take a look at these resources",
+    "For further reading, consider these resources",
+    "To deepen your understanding, consult these resources",
+    "For additional information, see these resources",
+    "To expand your knowledge, check out these resources",
+    "For more in-depth coverage, refer to these resources",
+    "To get a better grasp of the topic, check out these resources",
+    "For supplementary materials, look into these resources",
+    "To enhance your learning, explore these resources",
+    "To gain more insight, refer to these resources",
+    "To broaden your perspective, see these resources",
+    "For additional resources, consult these materials",
+    "To expand your understanding, explore these resources",
+    "For further exploration, see these resources",
+    "To continue your learning journey, consult these resources",
+    "To take your knowledge to the next level, refer to these resources",
+    "To go deeper into the subject, explore these resources",
+    "To access additional information, consider these resources"
+]
