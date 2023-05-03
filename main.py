87e505b27bfed1055fa9c2fafac6b430db9b4e69
hainghiem <hainghiem375@gmail.com>
2023-05-02 22:02:05 -0400

    added supabase for logging

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ai_tools import tool_describe_skills, tool_retrieve_company_info, tool_calc
 from ai_functions import load_urls_and_overwrite_index
 from consts import thinking_thoughts, demo_company_name
 from utils import extract_messages
+from supabase_wrapper import write_message_log
 
 load_dotenv()
 
@@ -20,6 +21,9 @@ app = App(token=bot_token)
 # Handle incoming DMs or channel messages
 @app.event("message")
 def handle_message_events(event, ack, say):
+    if (event["type"] != "message") or ("subtype" in event):
+        return
+
     # Acknowledge user's message
     ack()
     say(random.choice(thinking_thoughts))
@@ -31,10 +35,15 @@ def handle_message_events(event, ack, say):
         channel=channel, limit=10)
     messages_history.extend(extract_messages(conversation_history))
 
-    # Give the bot context of about the user (first name)
     user_id = event["user"]
     user_first_name = app.client.users_info(
         user=user_id)['user']['profile']['first_name']  # type: ignore
+
+    # Write message log to Supabase
+    write_message_log(user_name=user_first_name, message=event["text"])
+
+    # Give the bot context of about the user (first name)
+
     messages_history.append(
         {"type": "user", "message": f"""My name is {user_first_name} and I'll be asking questions about {demo_company_name} the company"""})
     messages_history.append(
@@ -56,6 +65,9 @@ def handle_message_events(event, ack, say):
             text=response,
         )
 
+        # Write message log to Supabase
+        write_message_log("AI", response)
+
         # Reaction emojis to get feedback from user
         reactions = ["thumbsup", "thumbsdown"]
         for reaction in reactions:
@@ -70,13 +82,16 @@ def handle_message_events(event, ack, say):
 @app.event("reaction_added")
 def handle_feedback(event, say):
     if (event['reaction'] == "+1"):
-        say(f"Thanks for your feedback! :{event['reaction']}:")
+        return
+        # say(f"Thanks for your feedback! :{event['reaction']}:")
     if (event['reaction'] == "-1"):
-        say(f"I'll do better next time! :disappointed:")
+        return
+        # say(f"I'll do better next time! :disappointed:")
     # TODO: save user's feedback to Google Sheets or something!
 
+    # Handle uploading new documentation using slash command
 
-# Handle uploading new documentation using slash command
+
 @app.command("/upload-new-doc")
 def handle_some_command(body, say, ack):
     ack()
